548c611acac3c1a2891c0b2af890842206de6932
aleksandrdiptan <diptan@huntflow.ru>
2022-11-28 22:04:32 +0300

    refactoring. - import choice move to file beginning - COMMENDATION move to outside of function at the global variable

--- scripts.py
+++ scripts.py
@@ -1,4 +1,16 @@
+from random import choice
+
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation, Subject
+
+COMMENDATION = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
+                     'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
+                     'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!',
+                     'Талантливо!', 'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!',
+                     'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!',
+                     'Здорово!', 'Это как раз то, что нужно!', 'Я тобой горжусь!',
+                     'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!',
+                     'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу! ',
+                     'Теперь у тебя точно все получится!']
 
 
 def get_schoolkid(schoolkid_full_name):
@@ -21,20 +33,10 @@ def remove_chastisement(schoolkid):
 
 
 def create_commendation(schoolkid_full_name, subject_name):
-    from random import choice
     child = get_schoolkid(schoolkid_full_name)
-    child_lessons = Lesson.objects.filter(group_letter=child.group_letter).filter(year_of_study=child.year_of_study)
+    child_lessons = Lesson.objects.filter(group_letter=child.group_letter, year_of_study=child.year_of_study)
     subject = Subject.objects.filter(title=subject_name).get(year_of_study=child.year_of_study)
     last_lesson = child_lessons.filter(subject__title=subject_name).order_by('-date').first()
     lesson_teacher = last_lesson.teacher
-    commendations = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
-                     'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
-                     'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!',
-                     'Талантливо!', 'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!',
-                     'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!',
-                     'Здорово!', 'Это как раз то, что нужно!', 'Я тобой горжусь!',
-                     'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!',
-                     'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу! ',
-                     'Теперь у тебя точно все получится!']
-    Commendation.objects.create(text=choice(commendations), created=last_lesson.date, schoolkid=child, subject=subject,
+    Commendation.objects.create(text=choice(COMMENDATION), created=last_lesson.date, schoolkid=child, subject=subject,
                                 teacher=lesson_teacher)
